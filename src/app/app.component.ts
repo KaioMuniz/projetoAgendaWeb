3eda7bb5c9e03d28e29103ef6a98f02b98662ab9
KaioMuniz <kkaioribeiro@gmail.com>
2025-06-02 20:53:00 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { CadastroTarefasComponent } from './components/pages/cadastro-tarefas/cadastro-tarefas.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    NavbarComponent,
-    CadastroTarefasComponent
+    NavbarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'projetoAgendaWeb';
+  //criando um objeto da classe httpclient
+
+  private http = inject(HttpClient)
+
+  //função executada sepre que a pagina for aberta
+  ngOnInit(){
+    
+
+      //executando uma requisição para o endpoint de consulta de categorias
+      this.http.get('http://localhost:8081/api/v1/categorias') 
+        .subscribe((dados) => { //capturando os dados obtidos
+            console.table(dados);
+        });
+  }
 }
