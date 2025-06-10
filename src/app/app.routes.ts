7acea1b67c32e6b055a17e05d17be9903b2cd895
KaioMuniz <kkaioribeiro@gmail.com>
2025-06-10 09:43:48 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,18 @@ import { CadastroTarefasComponent } from './components/pages/cadastro-tarefas/ca
 import { ConsultaTarefasComponent } from './components/pages/consulta-tarefas/consulta-tarefas.component';
 import { EdicaoTarefasComponent } from './components/pages/edicao-tarefas/edicao-tarefas.component';
 
+//Mapeamento das rotas de navegação do projeto
 export const routes: Routes = [
     {
-        path: 'pages/cadastro-tarefas',// rotas
+        path: 'pages/cadastro-tarefas', //rota de navegação
         component: CadastroTarefasComponent //componente
-
     },
     {
-        path: 'pages/consulta-tarefas',
-        component: ConsultaTarefasComponent
+        path: 'pages/consulta-tarefas', //rota de navegação
+        component: ConsultaTarefasComponent //componente
     },
     {
-        path: 'pages/edicao-tarefas',
-        component: EdicaoTarefasComponent
+        path: 'pages/edicao-tarefas/:id', //rota de navegação
+        component: EdicaoTarefasComponent //componente
     }
 ];
--- src/app/components/pages/consulta-tarefas/consulta-tarefas.component.ts
+++ src/app/components/pages/consulta-tarefas/consulta-tarefas.component.ts
@@ -3,13 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { environment } from '../../../../environments/environment';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-consulta-tarefas',
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './consulta-tarefas.component.html',
   styleUrl: './consulta-tarefas.component.css'
@@ -17,7 +19,8 @@ import { environment } from '../../../../environments/environment';
 export class ConsultaTarefasComponent {
 
   //atributos
-  tarefas: any[] = [] //array de obejtos
+  tarefas: any[] = []; //array de objetos
+  tarefaSelecionada: any; //objeto que armazena a tarefa selecionada
 
   //injeções de dependência
   http = inject(HttpClient);
@@ -38,8 +41,25 @@ export class ConsultaTarefasComponent {
     //fazendo uma requisição HTTP GET para a API[
     this.http.get(environment.apiTarefas + '/tarefas/' + dataMin + '/' + dataMax)
       .subscribe((response) => { //aguardando a resposta da API
-        //console.table(response); //exibindo os dados no console
+        //armazenando os dados obtidos da API
         this.tarefas = response as any[];
       })
   }
+
+  //função para capturar os dados da tarefa selecionada
+  onConfirm(tarefa: any) {
+    this.tarefaSelecionada = tarefa; //armazenando a tarefa selecionada
+  }
+
+  //função para realizar a exclusão da tarefa selecionada
+  onDelete() {
+    //fazendo uma requisição HTTP DELETE para a API
+    this.http.delete(environment.apiTarefas + "/tarefas/" + this.tarefaSelecionada.id)
+      .subscribe(() => { //aguardando a resposta da API
+        //removendo a tarefa selecionada do array de tarefas
+        this.tarefas = this.tarefas.filter(tarefa => tarefa.id !== this.tarefaSelecionada.id);
+        this.tarefaSelecionada = null; //limpando a tarefa selecionada
+      });
+  }
+
 }
